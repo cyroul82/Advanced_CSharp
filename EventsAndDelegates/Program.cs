6553cb3c75aab9cf512c0dd53f2621621375378a
Cyril Rat <cyril.rat@gmail.com>
2017-10-17 15:05:31 +0200

    Events and Delegates

---
+++ EventsAndDelegates/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+
+    /*
+     What are events and Why do we need them
+        - A mechanism for communication between objects
+        - Used in building Loosely Coupled Applications
+        - Helps extending applications
+
+         */
+
+    /*
+     * Delegates:
+     *   - Agreement / Contrat between Publisher and Subscriber
+     *   - Determines the signature of the event handler method in Subscriber
+     * 
+     */
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video() { Title = "Video 1" };
+            var videoEncoder = new VideoEncoder(); //Publisher
+            var mailService = new MailService(); //Subscriber
+            var messageService = new MessageService(); //Subscriber
+
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
+            videoEncoder.VideoEncoded += messageService.OnVideoEncoded;
+            
+
+            videoEncoder.Encode(video);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EventsAndDelegates/VideoEncoder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+namespace EventsAndDelegates
+{
+    public class VideoEventArgs : EventArgs
+    {
+        public Video Video { get; set; }
+
+    }
+
+    public class VideoEncoder
+    {
+        // 1 - Define a delegate
+        // 2 - Define an event based on that delegate
+        // 3 - Raise (or publish) the event 
+
+        //1
+        /*public delegate void VideoEncodedEventHandler(object sender, EventArgs args);*/
+        //public delegate void VideoEncodedEventHandler(object sender, VideoEventArgs args);  <------ Don't need anymore
+        //Hold a reference to a function that looks like this
+
+
+        // EventHandler
+        // EventHandler<TEventArgs>
+        public event EventHandler<VideoEventArgs> VideoEncoded;  //The same as above from the .NET
+
+        //2
+        //public event VideoEncodedEventHandler VideoEncoded; <-- Don't need anymore
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding Video");
+            Thread.Sleep(3000);
+            Console.WriteLine("Encoded Video...");
+
+            OnVideoEncoded(video);
+        }
+
+        //3 MSDN Convention, should be protected, virtual and void, and should start with the word on and then the name of the event
+        protected virtual void OnVideoEncoded(Video video)
+        {
+            if(VideoEncoded != null)
+            {
+                //VideoEncoded(this, EventArgs.Empty);
+                VideoEncoded(this, new VideoEventArgs() { Video = video });
+            }
+        }
+    }
+}
