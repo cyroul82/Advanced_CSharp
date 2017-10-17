ea0406eb59bbc00e113b379c682d7d43cdd9e893
Cyril Rat <cyril.rat@gmail.com>
2017-10-17 09:29:34 +0200

    Ajoutez des fichiers projet.

---
+++ Generics/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
