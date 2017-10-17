b3493a16e2cb923befcd483b630fcdd8c77eaf4d
Cyril Rat <cyril.rat@gmail.com>
2017-10-17 11:04:50 +0200

    Delegates

---
+++ Delegates/PhotoProcessor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Delegates
+{
+    public class PhotoProcessor
+    {
+        // 1 - declare the delegate
+        public delegate void PhotoFilterHandler(Photo photo);
+        
+        
+        // 2, Give an delegate as argument to the method
+        public void Process(string path, PhotoFilterHandler filterHandler)
+        {
+            var photo = Photo.Load(path);
+
+            // 3 - Pass the photo to the filter
+            // it means that this code doesn't know any longer what filter will be applied
+            // it is now the responsability of the client of this code
+            
+            filterHandler(photo);
+
+
+            /* ------------- code before delegate ----------*/
+            //var filters = new PhotoFilters();
+            //filters.ApplyBrightness(photo);
+            //filters.ApplyContrast(photo);
+            //filters.Resize(photo);
+
+            photo.Save();
+        }
+
+        //We can create delegate using default delegate with Action and Func (Action does not return anything as Func does)
+        public void Process(string path, Action<Photo> filterHandler)
+        {
+            var photo = Photo.Load(path);
+
+            filterHandler(photo);
+
+            photo.Save();
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Delegates
+{
+
+    /*
+     What is a Delegate ? 
+         a delegate is an object that knows how to call a method (or a group of methods)
+         Basiclay is a reference or a pointer to a function
+
+    Why do we need delegates ?
+        For designing extensible and flexible applications(eg: frameworks)
+
+    Alternatively we can use Interfaces !!!
+
+
+    Interfaces or Delegates ? (Personal Preferences, but MSDN Guideline says as follow)
+        Use a delegate when :
+            - An eventing design pattern is used
+            - The caller doesn't need to access other properties or methods on the object implementing the method
+    */
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var processor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+            PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+            filterHandler += filters.Resize;
+            filterHandler += ApplySaturation;
+            
+            processor.Process("urlphoto", filterHandler);
+
+
+            //the same but using default delegates
+            Action<Photo> filterHandler2 = filters.ApplyContrast;
+            processor.Process("urlphoto", filterHandler2);
+
+
+
+            Console.ReadLine();
+        }
+
+        //We can now create our own filter without recompiling the code 
+        static void ApplySaturation(Photo photo)
+        {
+            Console.WriteLine("Apply staturation");
+        }
+    }
+}
