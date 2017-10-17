3c98a45abb78e867e3b7ba6103271f51dabc9e55
Cyril Rat <cyril.rat@gmail.com>
2017-10-17 17:05:55 +0200

    extensions methods

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    /**
+     * What are Extension Methods ?
+     *  Allow us to add methods to an existing class without 
+     *      - changing its source code, or
+     *      - creating a new class that inherits from it
+     * */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "this is suppose very long blog post blah blah blah...";
+            var shortenedPost = post.Shorten(5);
+
+            IEnumerable<int> numbers = new List<int>() { 1, 5, 10, 8, 54, 47 };
+            var max = numbers.Max();
+
+            Console.WriteLine(max);
+
+            Console.WriteLine(shortenedPost);
+            Console.ReadLine();
+        }
+    }
+    
+}
---
+++ ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+    /*Careful to the namespace ...!!!*/
+
+    // 1 - Create a static class (to follow conventions)
+    public static class StringExtensions
+    {
+        // 2- Write Extension methods always static, 
+        // first argument should always be "this" + the type we're extending + the name
+
+        public static string Shorten(this String str, int numberOfWords)
+        {
+            if (numberOfWords < 0)
+                throw new ArgumentOutOfRangeException("numberOfWords should => 0");
+
+            if(numberOfWords == 0)
+                return "";
+
+            var words = str.Split(' ');
+
+            if (words.Length <= numberOfWords)
+                return str;
+
+            return string.Join(" ", words.Take(numberOfWords)) + "...";
+
+        }
+    }
+    
+}
