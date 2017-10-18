9eb000e663198d207d9e0c4d4b5327ee5d8d28ec
Cyril <cyril.rat@gmail.com>
2017-10-18 15:24:36 +0200

    interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+
+    /* What is an Interface ?
+     *  A language construct that is similar to a class (in terms of syntax) but is fundamentally different
+     *  
+     *  Why ?
+     *      To build loosely-coupled applications
+     *      
+     *      intertfaces and Testability
+     *      interfaces and Extensibily
+     *      interfaces are NOT for multiple inheritance
+     *      interfaces and polymorphism
+     *      
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
